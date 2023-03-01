f2b4744e7950ad77d34363ebb76fb9e1fdd9d9e1
Silvio_Sena <silviosenna.jl@gmail>
2023-03-01 11:40:14 -0300

    criando elementos no javaScript

--- projeto_inicial/assets/listaDeTarefas.js
+++
@@ -1,8 +0,0 @@
-const novaTarefa = document.querySelector('[data-form-button]');
-
-novaTarefa.addEventListener('click', () => { 
-    const input = document.querySelector('[data-form-input]');
-    const valor = input.value;
-    console.log('Fui clicado');
-})
-
